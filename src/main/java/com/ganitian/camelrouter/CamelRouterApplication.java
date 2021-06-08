28b5ca5f66597feefe7889927002165c0848cdb0
Rohan2596 <rohankadam662@gamil.com>
2021-06-08 14:40:56 +0530

    STARTED:Apache Camel Router for transaction.

---
+++ src/main/java/com/ganitian/camelrouter/CamelRouterApplication.java
@@ -0,0 +1,13 @@
+package com.ganitian.camelrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelRouterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ganitian/camelrouter/CamelRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganitian.camelrouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelRouterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
