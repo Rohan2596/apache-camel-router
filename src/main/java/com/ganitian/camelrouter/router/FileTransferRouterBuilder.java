58b0e69682945ae9dc220bfb916a5cc5bbaefdc8
Rohan2596 <rohankadam662@gamil.com>
2021-06-09 13:58:46 +0530

    REFRACTOR:Removing all preious code and unwanted imports from project.

--- src/main/java/com/ganitian/camelrouter/router/FileTransferRouterBuilder.java
+++ src/main/java/com/ganitian/camelrouter/router/FileTransferRouterBuilder.java
@@ -1,50 +1,29 @@
 package com.ganitian.camelrouter.router;
 
-import com.ganitian.camelrouter.dto.Transaction;
-import com.google.gson.Gson;
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-
-import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
+/*
+* STEP 1 : Creating basic File transfer for input to output file.
+* STEP 2 : Reading From File Using JAXB / Jackson file
+* STEP 3 : Establishing Communication with api endpoint.
+* STEP 4 : Sending Data to Api from file
+*
+* NOTE :-
+* 1) Organise the import according to the requirements of Project
+* 2) Follow the SOLID Principle as much as possible
+* 3) Import the jar/ dependencies which are required ( DON'T do the dumping of unwanted dependencies)
+*
+* */
 
-import static org.apache.camel.main.DefaultConfigurationConfigurer.LOG;
 
 @Component
 public class FileTransferRouterBuilder extends RouteBuilder {
+
 
     @Override
     public void configure() throws Exception {
 
 
-        from("file:files/input")
-               .log("${body}")
-               .process(new Processor() {
-                   @Override
-                   public void process(Exchange exchange) throws Exception {
-
-                       File file = exchange.getIn().getBody(File.class);
-                       LOG.info("Processing file: " + file.toString());
-                       System.out.println(file.getPath());
-                       Gson gson =new Gson();
-                   try(Reader reader = new FileReader(file.getPath())){
-                       Transaction transaction =gson.fromJson(reader,Transaction.class);
-                       exchange.getIn().setBody(transaction);
-                       System.out.println(transaction);
-                   }catch (IOException ioException){
-                       ioException.getMessage();
-                   }
-                   }
-
-               })
-                .log("${body}")
-                .to("log:body");
-
-    }
+         }
 }
